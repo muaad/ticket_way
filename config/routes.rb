fe4a4d500bc3ff26950606e026a1b6e4bd25b578
muaad <muadh24@gmail.com>
2015-05-27 22:32:18 +0300

    Generated scaffold for Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories
+
   resources :accounts
 
   root to: 'visitors#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527191858) do
+ActiveRecord::Schema.define(version: 20150527193029) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
