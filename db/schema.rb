99f2a95a049f588777189edd51ef22a83e45a327
muaad <muadh24@gmail.com>
2015-05-27 22:25:12 +0300

    Generated scaffold for Account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517201319) do
+ActiveRecord::Schema.define(version: 20150527191858) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
