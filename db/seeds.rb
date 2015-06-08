09f912ac0a04685c1242962e380e689353a76018
muaad <muadh24@gmail.com>
2015-06-09 00:04:34 +0300

    Added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+Category.find_or_create_by! name: "Technology"
+Category.find_or_create_by! name: "Wedding"
+Category.find_or_create_by! name: "Sport"
+Category.find_or_create_by! name: "Networking"
+Category.find_or_create_by! name: "Outdoors"
+Category.find_or_create_by! name: "Fitness"
+Category.find_or_create_by! name: "Entertainment"
+Category.find_or_create_by! name: "Politics"
+Category.find_or_create_by! name: "Religion"
